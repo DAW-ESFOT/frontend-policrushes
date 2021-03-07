c9d041eed20e538d5e890b5c3d3ef4d45a533529
Ronny Cajas <ronny.cajas@crifa.com>
2021-03-07 04:18:58 -0500

    fixes material ui styles issue with server side rendering

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,54 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import React from "react";
+//tema personalizado
+import { ThemeProvider } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+//tema global
+const theme = createMuiTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#D37586",
+      light: "rgb(72, 72, 212)",
+      dark: "pink",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "rgb(95	99	233	)",
+      light: "rgb(95	99	233	)",
+      dark: "rgb(95	99	233	)",
+      contrastText: "#fff",
+    },
+    background: {
+      paper: "white",
+      custom: "cyan",
+      default: "#FCF5F5",
+    },
+  },
+  customlink: {
+    background: "white",
+    borderRadius: 3,
+    border: 0,
+    color: "white",
+    height: 48,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  React.useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
