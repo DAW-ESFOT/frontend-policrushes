771e1b3d487df8fb87d7516b9c1778891cff9bce
Ronny Cajas <ronny.cajas@crifa.com>
2021-03-13 23:33:39 -0500

    login request improvements

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -42,27 +42,15 @@ function useAuthProvider() {
       console.log("response.data.user:", response);
       handleUser(response.data.user);
       Cookies.set("token", response.data.user.token);
-      return response.data.user;
+      return { status: "success" };
     } catch (error) {
       if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log("login error data:", error.response.data);
-        // onFail(error.response.data);
-        return error.response;
+        return { status: "error", messages: error.response };
       } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-        // onFail(error.response.data);
-        return error.request;
+        return { status: "error", messages: error.request };
       } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
+        return { status: "error", messages: error.config };
       }
-      console.log(error.config);
-      return null;
     }
   }
 
