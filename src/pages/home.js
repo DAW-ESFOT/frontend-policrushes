b1f75dc9b67501f2e6fd98cf41ff7d60eea25cd4
Ronny Cajas <ronny.cajas@crifa.com>
2021-03-13 23:56:36 -0500

    home token listener

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import withAuth from "@/hocs/withAuth";
 
 function Home() {
   useEffect(() => {
     setInterval(() => {
       console.log("token check");
-    }, 60*1000);
+    }, 60 * 1000);
   }, []);
 
   return <div>home</div>;
